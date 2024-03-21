8bcc00ae124259cd1665269398ec24f0dd2f06c0
Thiago-Ito <162365590+Thiago-Ito@users.noreply.github.com>
2024-03-21 16:57:11 -0300

    Update index.js

--- modelos/index.js
+++ modelos/index.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
     $.getJSON("modbus.json", function(data){
     console.log(data)
         $('.marca').html(data.marca);
-        
+        $('.modelo').html(data.modelo);
+    }).fail(function(){
+        console.log("Verifique o seu código")
     })
 })
