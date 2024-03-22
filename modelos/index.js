3bdce6493933b4dc1ea04da80893e5ea777c0c95
Thiago-Ito <162365590+Thiago-Ito@users.noreply.github.com>
2024-03-22 17:06:06 -0300

    Update index.js

--- modelos/index.js
+++ modelos/index.js
@@ -1,4 +1,1 @@
-     const meJSON = '{"Fabricante":"Khomp", "Modelo":"IED-302", "Aplicação":"Monitoramento"}'
-     const me = JSON.parse(meJSON);
-     console.log(me);    
-     Response.json(me);
+
