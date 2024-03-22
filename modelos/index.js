351de119f0d134d51d5d5cfbff217825d6cbfa79
Thiago-Ito <162365590+Thiago-Ito@users.noreply.github.com>
2024-03-22 17:03:33 -0300

    Update index.js

--- modelos/index.js
+++ modelos/index.js
@@ -1,10 +1,5 @@
-$(document).ready(function(){
-
-    $.getJSON("modbus.json", function(data){
-        
-    console.log(data)   
-        
-    }).fail(function(){
-        console.log("Verifique o seu código")
-    })
-})
+componentDidMount(){
+     fetch('https://industrial-devices.app.khomp.com/modelos')
+    .then(Response => Response.json())
+    .then(data => this.setState({items:data}));
+}
