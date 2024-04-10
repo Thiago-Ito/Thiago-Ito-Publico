26c07cc530d4a71a0282fa82dff416381c0a40c6
Thiago-Ito <162365590+Thiago-Ito@users.noreply.github.com>
2024-04-10 08:06:22 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const fs = require("fs");
 const http = require("http");
 const cors = require("cors");
-
+const bodyParser = require("body-parser");
 const app = express();
+//const routes = require("./routes/routes");
+
 app.use(cors());
 app.use(express.text({limit: "10mb"}));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+//app.use(routes);
 
 app.get("/", (req, res) => res.send({msg: "Serviço online............."}));
 
@@ -32,13 +35,14 @@ console.log(`Servidor rodando na porta ${port}` + ".............................
 //});
     
     var example_map = "\"{\"name\":\"MODELO 01\", \"id\": INPUT_ID}, \"total_packets\":5} {\"addr\":3004, \"func\":4, \"n_reg\":2} {\"addr\":4405, \"func\":3, \"n_reg\":1} {\"addr\":3025, \"func\":4, \"n_reg\":1} {\"addr\":4069, \"func\":3, \"n_reg\":1} {\"addr\":3604, \"func\":4, \"n_reg\":2}\"";
-   
-    app.post("/resposta", (req, res) => {
-        const result = JSON.parse(req.body);
-        res.send(example_map.replace("INPUT_ID", result[0].id));
-    });
-
     var mapa = require('./mapa_variaveis.json');
+
+    app.post("/resposta", (req, res) => {
+        const reqcliente = req.body;
+        console.log(reqcliente);
+        res.send(example_map[1].replace("INPUT_ID", "39"));
+    });
+    
     app.get('/resposta/', (req,res) => {res.send(mapa)});
 
     //const result = JSON.parse(req.body);    
